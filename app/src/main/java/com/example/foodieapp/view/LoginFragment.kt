82f7528c9dd43ba3b89211fcac531a1656b433d0
Dilek Özgül <dilekozgul9@gmail.com>
2021-10-17 00:49:58 +0300

    LoginFragment tamamlandı
    SignUp ve SignIn metotları tamamlandı. Giriş yaparken butonun klavyenin altında kalmaması için manifest dosyasına ekleme yaptım.

--- app/src/main/java/com/example/foodieapp/view/LoginFragment.kt
+++ app/src/main/java/com/example/foodieapp/view/LoginFragment.kt
@@ -5,10 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
 import com.example.foodieapp.R
+import com.example.foodieapp.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class LoginFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
+    private val binding get() = _binding!!
+    private var _binding: FragmentLoginBinding? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +30,59 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(layoutInflater,container,false)
+        val view = binding.root
+        return view
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
+        binding.btnSignIn.setOnClickListener { signIn(view)}
+        binding.btnSignUp.setOnClickListener { signUp(view)}
+
+
+
+        binding.btnForgotPassword.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToResetPasswordFragment("reset")
+            Navigation.findNavController(view).navigate(action)
+        }
+    }
+    fun signIn(view: View){
+        val email =  binding.etEmailAddress.text.toString()
+        val password = binding.etPassword.text.toString()
+        if(email.equals("") || password.equals("")){
+            Toast.makeText(requireContext(),"Enter email and password!", Toast.LENGTH_LONG).show()
+        }else{
+            auth.signInWithEmailAndPassword(email,password).addOnSuccessListener {
+                val action = LoginFragmentDirections.actionLoginFragmentToProfileFragment()
+                Navigation.findNavController(view).navigate(action)
+            }.addOnFailureListener {
+                Toast.makeText(requireContext(), it.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    fun signUp(view: View){
+        val email =  binding.etEmailAddress.text.toString()
+        val password = binding.etPassword.text.toString()
+        if(email.equals("") || password.equals("")){
+            Toast.makeText(requireContext(),"Enter email and password!", Toast.LENGTH_LONG).show()
+        }else{
+            auth.createUserWithEmailAndPassword(email,password).addOnSuccessListener {
+                val action = LoginFragmentDirections.actionLoginFragmentToProfileFragment()
+                Navigation.findNavController(view).navigate(action)
+            }.addOnFailureListener {
+                Toast.makeText(requireContext(), it.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+
+
 
 
 }
