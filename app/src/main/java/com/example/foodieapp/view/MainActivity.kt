40cf8fde14f3f7f82b0477029b326d6462e7fb0e
Dilek Özgül <dilekozgul9@gmail.com>
2021-10-17 01:34:28 +0300

    Fragment düzenleme
    statusbar, actionbar ve navigationbar renklerini ekranlara daha uygun hale gelmesi için değiştirdim.

--- app/src/main/java/com/example/foodieapp/view/MainActivity.kt
+++ app/src/main/java/com/example/foodieapp/view/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.foodieapp.view
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.example.foodieapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.statusBarColor)
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.parseColor("#181A1C")))
+        window.navigationBarColor = ContextCompat.getColor(this, R.color.navigationBarColor)
     }
 }
