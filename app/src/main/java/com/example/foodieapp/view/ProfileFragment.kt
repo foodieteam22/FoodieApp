81b3902d98c9ec97ec9f596b709a8a488fce064f
Dilek Özgül <dilekozgul9@gmail.com>
2021-10-17 16:18:52 +0300

    Profil fotografı seçme
    Galeriden fotograf seçmek için manifest dosyasına permission ekledim.
    Fotograf galeriden seçilmeden  kullanıcıya soruyor.
    Galeriden seçtiği fotografı image viewe yüklüyor ama henüz kaydetmiyor.

--- app/src/main/java/com/example/foodieapp/view/ProfileFragment.kt
+++ app/src/main/java/com/example/foodieapp/view/ProfileFragment.kt
@@ -1,14 +1,25 @@
 package com.example.foodieapp.view
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.Navigation
 import com.example.foodieapp.R
 import com.example.foodieapp.databinding.FragmentLoginBinding
 import com.example.foodieapp.databinding.FragmentProfileBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -18,6 +29,9 @@ class ProfileFragment : Fragment() {
     private lateinit var auth: FirebaseAuth
     private val binding get() = _binding!!
     private var _binding: FragmentProfileBinding? = null
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private  lateinit var permissonLauncher : ActivityResultLauncher<String>
+    var selectedPicture : Uri? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,10 +47,13 @@ class ProfileFragment : Fragment() {
         _binding = FragmentProfileBinding.inflate(layoutInflater,container,false)
         val view = binding.root
         return view
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        registerLauncher()
         auth = Firebase.auth
 
         binding.btnchangePassword.setOnClickListener {
@@ -54,6 +71,50 @@ class ProfileFragment : Fragment() {
             val action = ProfileFragmentDirections.actionProfileFragmentToLoginFragment()
             Navigation.findNavController(view).navigate(action)
         }
+
+        binding.imgProfile.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+                    Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission") {
+                        permissonLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    }.show()
+                }else{
+
+                    permissonLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+
+                }
+
+            } else{
+                val intentToGallery= Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+
+
+            }
+        }
+    }
+
+    private fun registerLauncher() {
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result->
+            if (result.resultCode == AppCompatActivity.RESULT_OK){
+                val intentFromResult = result.data
+                if(intentFromResult != null){
+                    selectedPicture = intentFromResult.data
+                    selectedPicture?.let {
+                        binding.imgProfile.setImageURI(it)
+                    }
+                }
+            }
+        }
+
+        permissonLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ result->
+            if(result){
+                val intentToGallery= Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            }else{
+                Toast.makeText(requireContext(), "Permisson needed!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 
 
