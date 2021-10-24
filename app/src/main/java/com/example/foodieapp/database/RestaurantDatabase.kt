527a4a5323d39d2f1097bf3e94b783d3ff91ce6a
SerkanKerik <serkan.kerik@gmail.com>
2021-10-24 20:40:30 +0300

    Restaurant Detail page changes

--- app/src/main/java/com/example/foodieapp/database/RestaurantDatabase.kt
+++ app/src/main/java/com/example/foodieapp/database/RestaurantDatabase.kt
@@ -5,14 +5,14 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = arrayOf(RestaurantEntry::class,CommentEntry::class,RatingEntry::class,RestaurantFeatureEntry::class),version = 1,exportSchema = false)
+@Database(entities = arrayOf(RestaurantEntry::class,CommentEntry::class,RatingEntry::class,UserEntry::class,RestaurantFeatureEntry::class),version = 1,exportSchema = false)
 abstract class RestaurantDatabase:RoomDatabase() {
 
     abstract fun resturantDao():RestaurantDao
     abstract fun commentDao():CommentDao
     abstract fun ratingDao():RatingDao
     abstract fun restaurantFeatureDao():RestaurantFeatureDao
-
+    abstract fun userDao():UserDao
    companion object
    {
        private var INSTANCE :RestaurantDatabase? =null
