136d23fe823f0bf15b16f2fd62dbbc63c79b7ebd
onurtoz <60987303+onurtoz@users.noreply.github.com>
2021-11-02 02:32:52 +0300

    Restaurant Action Fragment

--- app/src/main/java/com/example/foodieapp/viewadapter/ResturantAdapter.kt
+++ app/src/main/java/com/example/foodieapp/viewadapter/ResturantAdapter.kt
@@ -2,11 +2,19 @@ package com.example.foodieapp.viewadapter
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.foodieapp.R
 import com.example.foodieapp.databinding.RestaurantRowLayoutBinding
 import com.example.foodieapp.model.RestaurantModel
+import com.example.foodieapp.view.RestaurantDetailFragmentDirections
+import com.example.foodieapp.view.RestaurantFragmentDirections
 
 public class ResturantAdapter(
     val context: Context?,
@@ -18,6 +26,7 @@ public class ResturantAdapter(
             RestaurantRowLayoutBinding.inflate(
                 LayoutInflater.from(parent.context),
                 parent,
+
                 false
             )
         )
@@ -29,6 +38,13 @@ public class ResturantAdapter(
         fun bind(restModel: RestaurantModel) {
             binding.restaurantModel = restModel
             binding.executePendingBindings()
+
+            itemView.setOnClickListener(){
+                val position:Int = adapterPosition
+                val action = RestaurantFragmentDirections.actionRestaurantFragmentToRestaurantDetailFragment()
+                Navigation.findNavController(itemView).navigate(action)
+                Toast.makeText(itemView.context,restModel.name, Toast.LENGTH_LONG).show()
+            }
         }
     }
 
@@ -36,10 +52,10 @@ public class ResturantAdapter(
         val current = restData.get(position)
         holder.binding.restItemRecyclerView.layoutManager= LinearLayoutManager(context)
         holder.binding.restItemRecyclerView.adapter= RestaurantItemAdapter(current.items)
-
         holder.bind(current)
 
     }
+
 
 
     override fun getItemCount(): Int {
