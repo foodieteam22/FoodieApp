3ea9764e989337bd3bc70207965be59a4a551e6a
Dilek Özgül <dilekozgul9@gmail.com>
2021-11-02 00:05:33 +0300

    TableSchemeFragment
    masa düzeni resmi geliyor için resim gelior

--- app/src/main/java/com/example/foodieapp/view/TableSchemeFragment.kt
+++ app/src/main/java/com/example/foodieapp/view/TableSchemeFragment.kt
@@ -7,10 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.foodieapp.R
-import com.example.foodieapp.databinding.FragmentLoginBinding
-import com.example.foodieapp.databinding.FragmentRatingBinding
-import com.example.foodieapp.databinding.FragmentRestaurantDetailBinding
-import com.example.foodieapp.databinding.FragmentTableSchemeBinding
+import com.example.foodieapp.databinding.*
 import com.example.foodieapp.utils.downloadImage
 import com.example.foodieapp.utils.makePlaceholder
 import com.google.firebase.auth.ktx.auth
@@ -19,7 +16,9 @@ import com.google.firebase.ktx.Firebase
 
 class TableSchemeFragment : Fragment() {
 
-    private var binding: FragmentTableSchemeBinding? = null
+    private val binding get() = _binding!!
+    private var _binding: FragmentTableSchemeBinding? = null
+    //private var binding: FragmentTableSchemeBinding? = null
     private lateinit var args: TableSchemeFragmentArgs
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,25 +31,23 @@ class TableSchemeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         args = TableSchemeFragmentArgs.fromBundle(requireArguments())
-        // Inflate the layout for this fragment
+        _binding = FragmentTableSchemeBinding.inflate(layoutInflater,container,false)
+        val view = binding.root
+        return view
 
+       // binding = FragmentTableSchemeBinding.inflate(layoutInflater,container,false)
 
+      //  binding.apply {
 
-        binding = FragmentTableSchemeBinding.inflate(layoutInflater,container,false)
-
-        binding.apply {
-
-        }
-        return inflater.inflate(R.layout.fragment_table_scheme, container, false)
+      //  }
+      //  return inflater.inflate(R.layout.fragment_table_scheme, container, false)
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding?.imgTableScheme?.downloadImage(args.tableSchemeUrl, makePlaceholder(requireContext()))
-        binding.apply {
 
-        }
 
     }
 
