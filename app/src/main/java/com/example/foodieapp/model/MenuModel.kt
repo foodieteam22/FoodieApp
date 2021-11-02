6a4726cd200a373ca8fba4b3cb34490b8970e198
fgamzeatay <gamze.f.atay@gmail.com>
2021-11-02 19:38:18 +0300

    menu json model is changed

--- app/src/main/java/com/example/foodieapp/model/MenuModel.kt
+++ app/src/main/java/com/example/foodieapp/model/MenuModel.kt
@@ -5,8 +5,6 @@ import com.google.gson.annotations.SerializedName
 data class MenuModel (
     @SerializedName("id")
     var id: Long,
-   // @SerializedName("restaurantId")
-   // var restaurantId: Long,
     @SerializedName("name")
     var name: String,
     @SerializedName("menu-items")
